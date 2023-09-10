684d094c697e8a30b43cc72505f8ae3d2fe74252
Joelber Santos <joelber.santos@grupotpc.com>
2023-09-10 14:56:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using HostingControlExercise.Entities;
+
+namespace HostingControlExercise
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.Write("Room number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/MM/yyyy):");
+            DateTime checkOut = Convert.ToDateTime(Console.ReadLine());
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+
+        }
+    }
+}
