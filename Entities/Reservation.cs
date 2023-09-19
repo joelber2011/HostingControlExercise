638433d0b3eb051c0808d05ae3176fa3d6be493f
Joelber Santos <joelber.santos@grupotpc.com>
2023-09-18 21:17:29 -0300

    Adicionado DomainExceptions e otimização do código

--- Entities/Reservation.cs
+++ Entities/Reservation.cs
@@ -1,44 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HostingControlExercise.Entities.Exceptions;
 
-namespace HostingControlExercise.Entities
+namespace Course.Entities
 {
-    public class Reservation
+    class Reservation
     {
+
         public int RoomNumber { get; set; }
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
 
         public Reservation()
         {
-            
         }
 
         public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
             RoomNumber = roomNumber;
             CheckIn = checkIn;
             CheckOut = checkOut;
         }
 
-        public int Duration()
+        public double Duration()
         {
             TimeSpan duration = CheckOut.Subtract(CheckIn);
             return (int)duration.TotalDays;
         }
 
-        public void UpDateDates(DateTime checkIn, DateTime checkOut)
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
         {
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
             CheckIn = checkIn;
             CheckOut = checkOut;
         }
 
         public override string ToString()
         {
-            return "Room: "
+            return "Room "
                 + RoomNumber
                 + ", check-in: "
                 + CheckIn.ToString("dd/MM/yyyy")
@@ -46,7 +59,7 @@ namespace HostingControlExercise.Entities
                 + CheckOut.ToString("dd/MM/yyyy")
                 + ", "
                 + Duration()
-                + " nights.";
+                + " nights";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
-﻿using HostingControlExercise.Entities;
+﻿using Course.Entities;
+using HostingControlExercise.Entities.Exceptions;
 
-namespace HostingControlExercise
+namespace Course
 {
-    public class Program
+    class Program
     {
         static void Main()
         {
-            Console.Write("Room number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Check-in date (dd/MM/yyyy): ");
-            DateTime checkIn = Convert.ToDateTime(Console.ReadLine());
+            try
+            {
+                Console.Write("Room number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = Convert.ToDateTime(Console.ReadLine());
 
-            Console.Write("Check-out date (dd/MM/yyyy):");
-            DateTime checkOut = Convert.ToDateTime(Console.ReadLine());
+                Reservation reservation = new (number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
 
-            Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = Convert.ToDateTime(Console.ReadLine());
 
-
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
         }
     }
 }
